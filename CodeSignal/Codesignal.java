cf13309fda5423ca4cd35ebb07bd33e0543aac2e
Ketan <ketantoo@gmail.com>
2018-11-23 08:28:54 +0530

    23 Nov 2018

--- CodeSignal/Codesignal.java
+++ CodeSignal/Codesignal.java
@@ -2329,4 +2329,11 @@ int sumOfPowers(int n, int divisor) {
     return s;
 }
 
+//////////////////////////////////
 
+int fibonacciNumber(int n) {
+    int[] fib = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987};
+    return fib[n];
+}
+
+//////////////////////////////////
