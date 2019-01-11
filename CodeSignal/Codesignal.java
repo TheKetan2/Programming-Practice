78d94869ad7ebbab6e1e2a2699492ef9570c0baf
Ketan <ketantoo@gmail.com>
2019-01-11 12:36:41 +0530

    11 Jan 2019

--- CodeSignal/Codesignal.java
+++ CodeSignal/Codesignal.java
@@ -4468,33 +4468,44 @@ class Helper {
     }
 
     /////////////////////////////////////////////
-  boolean checkEqualFrequency(int[] inputArray) {
+    boolean checkEqualFrequency(int[] inputArray) {
 
-    int numberOfEqual = 1;
-  
-    Arrays.sort(inputArray);
-  
-    while (numberOfEqual < inputArray.length
-        && inputArray[numberOfEqual - 1] == inputArray[numberOfEqual]) {
-      numberOfEqual++;
-    }
-  
-    if (inputArray.length % numberOfEqual != 0) {
-      return false;
-    }
-  
-    for (int i = 0; i < inputArray.length; i += numberOfEqual) {
-      if (i != 0 && inputArray[i] == inputArray[i - 1]) {
-        return  false ;
-      }
-      for (int j = i + 1; j < i + numberOfEqual; j++) {
-        if (inputArray[j] != inputArray[j - 1]) {
-          return false;
+        int numberOfEqual = 1;
+
+        Arrays.sort(inputArray);
+
+        while (numberOfEqual < inputArray.length && inputArray[numberOfEqual - 1] == inputArray[numberOfEqual]) {
+            numberOfEqual++;
         }
-      }
-    }
-  
-    return true;
-  }
 
-///////////////////////////////////////////////////
+        if (inputArray.length % numberOfEqual != 0) {
+            return false;
+        }
+
+        for (int i = 0; i < inputArray.length; i += numberOfEqual) {
+            if (i != 0 && inputArray[i] == inputArray[i - 1]) {
+                return false;
+            }
+            for (int j = i + 1; j < i + numberOfEqual; j++) {
+                if (inputArray[j] != inputArray[j - 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    ///////////////////////////////////////////////////
+boolean checkIncreasingSequence(int[] seq) {
+    if(seq.length <=1)
+        return true;
+    for(int i = 1; i<seq.length; i++){
+        if(seq[i-1] >= seq[i])
+            return false;
+    }
+    return true;
+    
+}
+
+//////////////////////////////////////////////
