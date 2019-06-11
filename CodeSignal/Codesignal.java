33547f63d336a91e80f5d51432c30fe2b6778a00
Ketan <ketantoo@gmail.com>
2019-06-11 15:24:54 +0530

    11 June 2019

--- CodeSignal/Codesignal.java
+++ CodeSignal/Codesignal.java
@@ -829,6 +829,28 @@ boolean subsetsSequence(int[][] sets) {
   
 
     ///////////////////////////////////////
+
+    boolean isMAC48Address(String inputString) {
+
+  for (int i = 0; i < inputString.length(); i++) {
+    if (i % 3 == 2) {
+      if (inputString.charAt(i) != '-') {
+        return false;
+      }
+    }
+    else {
+      char sym = inputString.charAt(i);
+      if (!('0' <= sym && sym <= '9' || 'A' <= sym && sym <= 'F')) {
+        return false;
+      }
+    }
+  }
+
+  return inputString.length() == 17;
+}
+
+///////////////////////////////////////
+
         int maximizeNumberRoundness(int n) {
         int tmp = n;
         int zeros = 0;
