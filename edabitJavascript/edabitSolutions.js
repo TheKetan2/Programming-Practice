eb01aedb8e08d92d121c05cd68155b9a40d3b14d
Ketan <ketantoo@gmail.com>
2019-11-13 23:29:14 +0530

    13 nov 2019

--- edabitJavascript/edabitSolutions.js
+++ edabitJavascript/edabitSolutions.js
@@ -55,3 +55,124 @@ function oddEven(num) {
 function check(arr, el) {
   return arr.indexOf(el) !== -1;
 }
+
+// Create a function that converts a date formatted as MM/DD/YYYY to a format required as YYYYDDMM. The parameter userDate and the return value are strings.
+
+// Examples
+// formatDate("11/12/2019") ➞ "20191211"
+
+// formatDate("12/31/2019") ➞ "20193112"
+
+function formatDate(userDate) {
+  return userDate
+    .split("/")
+    .reverse()
+    .join("");
+}
+
+// Function to return exponential value
+
+function calculateExponent(num, exp) {
+  // return Math.pow(num, exp)
+  return num ** exp;
+}
+
+// Create a function that takes an array and a string as arguments and return the index of the string.
+
+// Examples
+// find_index(["hi", "edabit", "fgh", "abc"], "fgh") ➞ 2
+
+// find_index(["Red", "blue", "Blue", "Green"], "blue") ➞ 1
+
+// find_index(["a", "g", "y", "d"], "d") ➞ 3
+
+// find_index(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple") ➞ 0
+
+function find_index(arr, str) {
+  return arr.indexOf(str);
+}
+
+// Create a function that takes in three arguments (prob, prize, pay) and returns true if prob * prize > pay; otherwise return false.
+
+// To illustrate, profitableGamble(0.2, 50, 9) should yield true, since the net profit is 1 (0.2 * 50 - 9), and 1 > 0.
+
+function profitableGamble(prob, prize, pay) {
+  return prob * prize > pay;
+}
+
+// Tom is a very methodic guy that loves geometry and pizza: he loves them so much that, before eating a pizza, he calculates its radius and its height. Now, he wants to know from you the total volume of pizza that he swallowed!
+
+// You are given the two parameters that Tom measured:
+
+// radius
+// height
+// He tells you that if you multiply the height for the square of the radius and multiply the result for the mathematical constant π (Pi), you will obtain the total volume of the pizza. Implement a function that returns the volume of the pizza as a whole number, rounding it to the nearest integer (and rounding up for numbers with .5 as decimal part).
+
+function volPizza(radius, height) {
+  return Math.round(radius * radius * 3.14159 * height);
+}
+
+// Write a function that returns true if k^k == n for input (n, k).
+
+// Examples
+// kToK(4, 2) ➞ true
+
+// kToK(387420489, 9) ➞ true
+// // 9^9 == 387420489
+
+// kToK(3124, 5) ➞ false
+
+// kToK(17, 3) ➞ false
+
+function kToK(n, k) {
+  return k ** k === n;
+}
+
+// Write a function that validates whether two strings are identical. Make it case insensitive.
+
+// Examples
+// match("hello", "hELLo") ➞ true
+
+// match("motive", "emotive") ➞ false
+
+// match("venom", "VENOM") ➞ true
+
+// match("mask", "mAskinG") ➞ false
+
+function match(s1, s2) {
+  return s1.toLowerCase() === s2.toLowerCase();
+}
+
+function stackBoxes(n) {
+  return n ** 2;
+}
+
+function equalSlices(total, people, each) {
+  return total >= people * each;
+}
+
+function operation(num1, num2) {
+  if (num1 + num2 === 24) return "added";
+  if (num1 * num2 === 24) return "multiplied";
+  if (num1 - num2 === 24 || num2 - num1 === 24) return "subtracted";
+  if (num1 / num2 === 24 || num2 / num1 === 24) return "divided";
+  return null;
+}
+
+function negate(arr) {
+  return arr.map(item => -item);
+}
+
+function programmers(one, two, three) {
+  let arr = [one, two, three].sort((a, b) => a - b);
+  return arr[2] - arr[0];
+}
+
+function isEqual(num1, num2) {
+  if (typeof num1 === "string" || typeof num2 === "string") return false;
+  return num1 === num2;
+}
+function diffMaxMin(arr) {
+  arr.sort((a, b) => a - b);
+  return arr[arr.length - 1] - arr[0];
+}
