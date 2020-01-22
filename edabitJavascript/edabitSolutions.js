f1d76734e3f40205f6ad7817e3a9eed56f857d8e
Ketan <ketantoo@gmail.com>
2020-01-22 20:17:06 +0530

    factorize number

--- edabitJavascript/edabitSolutions.js
+++ edabitJavascript/edabitSolutions.js
@@ -2043,3 +2043,11 @@ function uniqueSort(arr) {
   }
   return sol.sort((a, b) => a - b);
 }
+
+function factorize(num) {
+  let sol = [];
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) sol.push(i);
+  }
+  return sol;
+}
