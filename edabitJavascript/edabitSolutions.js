29d5a6cab69808a4f7587345c4127fbc14e5f949
Ketan <ketantoo@gmail.com>
2020-01-24 21:53:53 +0530

    missing num

--- edabitJavascript/edabitSolutions.js
+++ edabitJavascript/edabitSolutions.js
@@ -2273,3 +2273,7 @@ function validatePIN(pin) {
     ? pin.replace(/[0-9]/g, "").length === 0
     : false;
 }
+
+function missingNum(arr) {
+  return 55 - arr.reduce((acc, curr) => acc + curr);
+}
