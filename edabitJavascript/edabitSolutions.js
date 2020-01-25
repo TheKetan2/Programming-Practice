68b01c78446834cd40dc08f576015e2812c7ea25
Ketan <ketantoo@gmail.com>
2020-01-25 21:55:07 +0530

    is anagram

--- edabitJavascript/edabitSolutions.js
+++ edabitJavascript/edabitSolutions.js
@@ -2289,3 +2289,18 @@ function pizzaPoints(customers, minOrders, minPrice) {
   }
   return sol.sort();
 }
+
+function isAnagram(s1, s2) {
+  let str1 = s1
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+  let str2 = s2
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+  return str1 === str2;
+}
+
