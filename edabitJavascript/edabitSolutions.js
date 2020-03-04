d6b9178e065f7522dfe1d1df3d20319d4b3e373a
Ketan <ketantoo@gmail.com>
2020-03-04 19:56:40 +0530

    Unique Character Mapping

--- edabitJavascript/edabitSolutions.js
+++ edabitJavascript/edabitSolutions.js
@@ -3248,3 +3248,30 @@ function twins(arr) {
   }
   return -1;
 }
+
+function characterMapping(str) {
+  let value = 0;
+  let objMap = {};
+  let sol = [];
+  for (ch of [...str]) {
+    if (objMap[ch] === undefined) {
+      if (value === 0) {
+        sol.push(value);
+        objMap[ch] = value;
+        value++;
+      } else {
+        if (value === 0) {
+          value++;
+          objMap[ch] = value;
+        } else {
+          objMap[ch] = value;
+          value++;
+        }
+        sol.push(objMap[ch]);
+      }
+    } else {
+      sol.push(objMap[ch]);
+    }
+  }
+  return sol;
+}
