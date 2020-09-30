4398252cc5dc70285b1dd1c8b9109bd8a46b13f1
Ketan <ketantoo@gmail.com>
2020-09-30 10:42:44 +0530

    sum of array element

---
+++ geeks for geeks/Sum of array elements.py
@@ -0,0 +1,12 @@
+"""
+https://practice.geeksforgeeks.org/problems/sum-of-array-elements/1/?undefined
+Github: theKetan2
+
+"""
+
+num = int(input())
+for i in range(num):
+    len = int(input())
+    elem = input().strip().split(" ")
+    print(sum(list(map(int, elem))))
+        
