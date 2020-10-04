4e9853c021c1f0798d80246b3fed13c1f25570af
Ketan <ketantoo@gmail.com>
2020-10-04 23:35:30 +0530

    removeCoveredIntervals

--- edabitJavascript/edabitSolutions.js
+++ edabitJavascript/edabitSolutions.js
@@ -3529,3 +3529,19 @@ function addNums(nums) {
     .split(",")
     .reduce((a, b) => parseInt(a) + parseInt(b), 0);
 }
+
+var removeCoveredIntervals = function (intervals) {
+  let k = 0;
+  intervals.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+  for (let i = 0, j = 1; j < intervals.length; j++) {
+    if (
+      intervals[i][0] <= intervals[j][0] &&
+      intervals[i][1] >= intervals[j][1]
+    ) {
+      k++;
+    } else {
+      i = j;
+    }
+  }
+  return intervals.length - k;
+};
