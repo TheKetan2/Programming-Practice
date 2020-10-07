f0219dff6533dbc890068d737e6d5a2f317fa1a6
Ketan <ketantoo@gmail.com>
2020-10-07 11:56:26 +0530

    betweenTwo

--- CP Study Guide/collatzalgo.cpp
+++ CP Study Guide/collatzalgo.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     long long n;
     cin >> n;
+
     while(true){
         cout << n << " ";
         if(n==1)
---
+++ HackerRank/betweenTwo.js
@@ -0,0 +1,32 @@
+/**
+ * https://www.hackerrank.com/challenges/between-two-sets/problem
+ * GitHub: theketan2
+ */
+
+function getTotalX(a, b) {
+  // Write your code here
+  a.sort((a, b) => a - b);
+  b.sort((a, b) => a - b);
+  let sol = [];
+  let factor = a[a.length - 1];
+  while (true) {
+    let flag = true;
+    for (let i = 0; i < a.length; i++) {
+      console.log(factor);
+      if (factor % a[i] !== 0) {
+        flag = false;
+        break;
+      }
+      //console.log("factor: ",b.filter(num => num%factor === 0));
+      if (b.filter((num) => num % factor === 0).length !== b.length) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) sol.push(factor);
+    factor += a[a.length - 1];
+    if (factor > b[0]) break;
+  }
+  // console.log(sol)
+  return sol.length;
+}
