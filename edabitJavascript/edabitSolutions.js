1609cc5f90a11f97ab34c4eea05bf531ae0db78c
Ketan <ketantoo@gmail.com>
2020-11-06 09:45:10 +0530

    tempConversion

--- edabitJavascript/edabitSolutions.js
+++ edabitJavascript/edabitSolutions.js
@@ -4028,3 +4028,12 @@ function minLength(arr, n) {
   }
   return found ? sol : -1;
 }
+
+function tempConversion(celsius) {
+  return celsius + 273.15 < 0
+    ? "Invalid"
+    : [
+        Number(((celsius * 9) / 5 + 32).toFixed(2)),
+        Number((celsius + 273.15).toFixed(2)),
+      ];
+}
--- edabitJavascript/temp.js
+++ edabitJavascript/temp.js
@@ -1,16 +1,8 @@
-function minLength(arr, n) {
-  let sol = arr.length;
-  let found = false;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j <= arr.length; j++) {
-      let sumInRange = arr.slice(i, j).reduce((acc, curr) => acc + curr);
-      if (sumInRange > n) {
-        found = true;
-        if (arr.slice(i, j).length < sol) {
-          sol = arr.slice(i, j).length;
-        }
-      }
-    }
-  }
-  return found ? sol : -1;
+function tempConversion(celsius) {
+  return celsius + 273.15 < 0
+    ? "Invalid"
+    : [
+        Number(((celsius * 9) / 5 + 32).toFixed(2)),
+        Number((celsius + 273.15).toFixed(2)),
+      ];
 }
