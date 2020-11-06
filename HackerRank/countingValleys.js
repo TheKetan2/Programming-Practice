d85d166865142633524ed566ae76d8f323102e7d
Ketan <ketantoo@gmail.com>
2020-11-06 20:19:55 +0530

    countingValleys

--- HackerRank/countingValleys.js
+++ HackerRank/countingValleys.js
@@ -7,15 +7,12 @@
 function countingValleys(steps, path) {
   // Write your code here
   let seaLevel = 0;
-  let arr = [];
   let temp = [];
   for (let ch of [...path]) {
     if (ch === "U") {
       seaLevel += 1;
-      arr.push(seaLevel);
     } else if (ch === "D") {
       seaLevel -= 1;
-      arr.push(seaLevel);
     }
     if (!temp.length && seaLevel <= 0) {
       temp.push(seaLevel);
