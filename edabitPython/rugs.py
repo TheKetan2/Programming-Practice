1a03de68834fe039fe3aab141b112cb3cc212c99
Ketan <ketantoo@gmail.com>
2021-02-01 12:20:25 +0530

    make_rug

--- edabitPython/rugs.py
+++ edabitPython/rugs.py
@@ -4,3 +4,7 @@ def make_rug(*args):
 		return ["#"*args[1] for i in range(args[0])]
 	else:
 		return [args[2]*args[1] for i in range(args[0])]
+
+def make_rug(m,n,s="#"):
+	return [s*n for i in range(m)]
+	
