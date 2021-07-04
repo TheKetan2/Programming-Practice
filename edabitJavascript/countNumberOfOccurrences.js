9979c6f0381937dfa410206b41ec1c45539bbc7e
Ketan <ketantoo@gmail.com>
2021-07-04 17:56:12 +0530

    countNumberOfOccurrences

---
+++ edabitJavascript/countNumberOfOccurrences.js
@@ -0,0 +1,13 @@
+function countNumberOfOccurrences(obj) {
+	let values = Object.values(obj)
+	let sol = {}
+	for(let i=0; i<values.length; i++){
+		let key = values[i]
+		if(sol[key]){
+			sol[key] +=1;
+		}else{
+			sol[key] = 1;
+		}
+	}
+	return sol;
+}
