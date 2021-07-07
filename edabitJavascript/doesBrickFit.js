efa8f48c249b7a645a583d692dc8a66e4766e013
Ketan <ketantoo@gmail.com>
2021-07-07 12:42:41 +0530

    doesBrickFit

---
+++ edabitJavascript/doesBrickFit.js
@@ -0,0 +1,9 @@
+// (a,b,c) -- dimensions of the brick
+// (w,h) -- dimensions of the hole
+function doesBrickFit(a,b,c, w,h) {
+	let brick = [a,b,c]
+	let hole = [w,h]
+	brick.sort()
+	hole.sort()
+	return brick[0]<=hole[0] && brick[1]<=hole[1]
+}
