b991eeec62f696056fba91ff9139a9ab8159ccb5
Ketan <ketantoo@gmail.com>
2021-07-10 12:58:08 +0530

    function longest7SegmentWord(arr) {

---
+++ edabitJavascript/longest7SegmentWord.js
@@ -0,0 +1,12 @@
+function longest7SegmentWord(arr) {
+	let result = arr.filter(word => !word.includes("k") 
+			 &&!word.includes("k")
+			 &&!word.includes("m")
+			 &&!word.includes("v")
+			 &&!word.includes("w")
+			 &&!word.includes("x"))
+	result.sort((a,b)=> b.length-a.length)
+	return result[0]?result[0]:""
+	
+}
+//Letters which do not fit on a 7 segment display are k, m, v, w and x.
