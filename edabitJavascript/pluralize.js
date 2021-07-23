18b4761358dd9fc19833f8acbd5afe957ce10f2d
Ketan <ketantoo@gmail.com>
2021-07-23 09:22:16 +0530

    pluralize

---
+++ edabitJavascript/pluralize.js
@@ -0,0 +1,12 @@
+function pluralize(arr) {
+	let obj = {}
+	for(let item of arr){
+		if(obj[item]){
+			obj[item]+=1;
+		}else{
+			obj[item] = 1;
+		}
+	}
+	return Object.keys(obj).map(key=> obj[key] >1?`${key}s`:key)
+}
+	
