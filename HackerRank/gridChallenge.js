45c335f2e92cc8674c638192b4b3e5a8efa5d7c9
Ketan <ketantoo@gmail.com>
2021-07-26 22:32:35 +0530

    gridChallenge

---
+++ HackerRank/gridChallenge.js
@@ -0,0 +1,25 @@
+
+function gridChallenge(grid) {
+    // Write your code here
+    let sortedGrid = grid.map(item => {
+        let temp = item.split("").sort()
+        return temp;
+    })
+    // console.log(sortedGrid)
+    let columnSorted = []
+    for(let i = 0; i<sortedGrid[0].length; i++){
+        let temp = "";
+        for(let j = 0; j<sortedGrid.length; j++){
+            // console.log(sortedGrid[j])
+            temp += sortedGrid[j][i];
+        }
+        columnSorted.push(temp)
+    }
+    console.log(columnSorted);
+    let filteredColumn = columnSorted.filter(item => {
+        let t = item.split("").sort().join("")
+        console.log(t," : ",item)
+        return item === t;
+    })
+    return filteredColumn.length === columnSorted.length?"YES":"NO";
+}
