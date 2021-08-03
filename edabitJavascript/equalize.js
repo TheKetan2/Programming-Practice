511af3976d08147f4dbec90009df42ab3fc66243
Ketan <ketantoo@gmail.com>
2021-08-03 22:55:53 +0530

    equalize

---
+++ edabitJavascript/equalize.js
@@ -0,0 +1,27 @@
+function equalize(arr, c) {
+	let obj = {}
+	let max = 0,sol=0;
+	for(let item of arr){
+		if(obj[item]){
+			obj[item]+=1;
+		}else{
+			obj[item] = 1;
+		}
+		if(max<obj[item]){
+			max = item
+		}
+	}
+	while(arr.length){
+		let temp = c;
+		let flag = false;
+		while(temp--){
+			if(arr.pop()!== max){
+				flag = true;
+			}
+		}
+		if(flag){
+			sol++;
+		}
+	}
+	return sol-1;
+}
