2070d8b07d86f19ec05fe6cac6d65cfc15657250
Ketan <ketantoo@gmail.com>
2021-08-05 19:14:05 +0530

    actualMemorySize

---
+++ edabitJavascript/actualMemorySize.js
@@ -0,0 +1,10 @@
+function actualMemorySize(ms) {
+    let space = parseInt(ms.replace(/[^0-9]/g, ""))
+    if (ms.includes("MB")) {
+        return (space - space * (7 / 100)).toFixed(0) + "MB"
+    } else {
+        return (space - space * (7 / 100)) < 1 ?
+            Math.round((space - space * (7 / 100)) * 1000) + "MB" :
+            (space - space * (7 / 100)).toFixed(2) + "GB"
+    }
+}
