cd3aaff46cc47b6663a2b9c0b036e9b800963e7e
Ketan <ketantoo@gmail.com>
2021-08-06 20:56:38 +0530

    insertWhitespace

---
+++ edabitJavascript/insertWhitespace.js
@@ -0,0 +1,11 @@
+function insertWhitespace(s) {
+	let sol = ""
+	for(let i = 0;i<s.length;i++){
+		if(s[i]>="A" && s[i]<="Z"){
+			sol +=(" "+s[i])
+		}else{
+			sol +=s[i]
+		}
+	}
+	return sol.trim()
+}
