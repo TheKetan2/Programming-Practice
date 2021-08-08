6b1daa90e4eec6ee4df4931d62212e4cc0fdfe85
Ketan <ketantoo@gmail.com>
2021-08-08 20:47:20 +0530

    sum2

--- edabitJavascript/insertWhitespace.js
+++ edabitJavascript/insertWhitespace.js
@@ -2,9 +2,9 @@ function insertWhitespace(s) {
 	let sol = ""
 	for(let i = 0;i<s.length;i++){
 		if(s[i]>="A" && s[i]<="Z"){
-			sol +=(" "+s[i])
+            sol += (" " + s[i]);
 		}else{
-			sol +=s[i]
+            sol += s[i];
 		}
 	}
 	return sol.trim()
---
+++ edabitJavascript/sum2.js
@@ -0,0 +1,21 @@
+const num={"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"0":0,undefined:0};
+function sum2(a, b) {
+	let numA = [...a];
+	let numB = [...b];
+	let sol = "";
+	let carry = 0;
+	while(numA.length || numB.length){
+		let tempA = num[numA.pop()];
+		let tempB = num[numB.pop()];
+		let sum = tempA+tempB+carry;
+		if(sum>9){
+			carry = Math.floor(sum/10);
+			sol = sum%10 + sol;
+		}else{
+			sol = sum + sol;
+			carry = 0;
+		}
+		//console.log(`${tempA} + ${tempB}+${carry} = ${sum}, ${sol}`,carry)
+	}
+	return carry?carry+sol:sol;
+}
