f76361723aa426e332e04fa7a47e602d5a6bbc4c
Ketan <ketantoo@gmail.com>
2021-08-10 14:56:12 +0530

    vendingMachine

--- edabitJavascript/vendingMachine.js
+++ edabitJavascript/vendingMachine.js
@@ -1,3 +1,23 @@
+/**
+ * url: https://edabit.com/challenge/PYXbvQh9W3c9i72xx
+ * @param {
+ * } products 
+ * @param {*} money 
+ * @param {*} productNumber 
+ * @returns 
+ */
+const products = [
+  { number: 1, price: 100, name: 'Orange juice' },
+  { number: 2, price: 200, name: 'Soda' },
+  { number: 3, price: 150, name: 'Chocolate snack' },
+  { number: 4, price: 250, name: 'Cookies' },
+  { number: 5, price: 180, name: 'Gummy bears' },
+  { number: 6, price: 500, name: 'Condoms' },
+  { number: 7, price: 120, name: 'Crackers' },
+  { number: 8, price: 220, name: 'Potato chips' },
+  { number: 9, price: 80,  name: 'Small snack' },
+];
+
 function vendingMachine(products, money, productNumber) {
 	if(productNumber<=0 || productNumber>9){
 		return 'Enter a valid product number';
