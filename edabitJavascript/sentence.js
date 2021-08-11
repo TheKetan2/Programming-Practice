5a6b138b6a99c7c1fa79c1a0f267a53e0d3429c6
Ketan <ketantoo@gmail.com>
2021-08-11 22:44:35 +0530

    sentence

---
+++ edabitJavascript/sentence.js
@@ -0,0 +1,16 @@
+function sentence(words) {
+    let sol = "";
+    let len = words.length - 1;
+    words.forEach((item, i) => {
+				let isVowel = "aeiouAEIOU".includes(item[0]);
+        if (i == 0) {
+            sol +=  isVowel ? `An ${item}` : `A ${item}`
+        } else if (i === len) {
+            sol += isVowel ? ` and an ${item}.` : ` and a ${item}.`
+        } else {
+            sol += isVowel ? `, an ${item}` : `, a ${item}`
+        }
+    })
+    console.log(sol)
+    return sol;
+}
