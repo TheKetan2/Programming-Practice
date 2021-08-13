01a4024b5b2a373d5c08d9bdf05e26418d909c97
Ketan <ketantoo@gmail.com>
2021-08-13 20:18:23 +0530

    lcmHard

---
+++ edabitJavascript/lcmHard.js
@@ -0,0 +1,11 @@
+function lcm(nums) {
+	let max = Math.max(...nums);
+	let len = nums.length, tempMax = max;
+	while(true){
+		let temp = nums.filter(num => tempMax%num==0)
+		if(temp.length === len){
+			return tempMax;
+		}
+		tempMax +=max;
+	}
+}
