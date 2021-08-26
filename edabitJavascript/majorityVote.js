934e0422a882c6a07885dd8bc7fc104c225e5bba
Ketan <ketantoo@gmail.com>
2021-08-26 20:17:00 +0530

    majorityVote

---
+++ edabitJavascript/majorityVote.js
@@ -0,0 +1,20 @@
+function majorityVote(arr) {
+  let obj = {};
+  if (!arr.length) {
+    return null;
+  }
+  let set = new Set(arr);
+  if (set.size === 1) {
+    return arr[0];
+  }
+  for (let vote of arr) {
+    if (obj[vote]) {
+      obj[vote] += 1;
+    } else {
+      obj[vote] = 1;
+    }
+  }
+  console.log(obj);
+  let sol = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+  return sol[0][1] == sol[sol.length - 1][1] ? null : sol[0][0];
+}
