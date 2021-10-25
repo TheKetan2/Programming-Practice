61c701d851e555ddbda4e728c4bb69c5b8e52674
theketan2 <ketantoo@gmail.com>
2021-10-25 11:41:19 +0530

    getNumberOfApples

---
+++ edabitJavascript/getNumberOfApples.js
@@ -0,0 +1,5 @@
+function getNumberOfApples(n, p) {
+	let finished = n * (parseInt(p)/100);
+	let remaining = Math.floor(n-finished);
+	return remaining?remaining:"The children didn't get any apples";
+}
