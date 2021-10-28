618cd85a5b37b88baac9969fa0ee12817c38067e
theketan2 <ketantoo@gmail.com>
2021-10-28 11:38:20 +0530

    sumMissingNumbers

---
+++ edabitJavascript/sumMissingNumbers.js
@@ -0,0 +1,11 @@
+function sumMissingNumbers(arr) {
+	let min = Math.min(...arr);
+	let max = Math.max(...arr);
+	let sum = 0;
+	for(let i = min;i<max; i++){
+		if(!arr.includes(i)){
+			sum+=i;
+		}
+	}
+	return sum;
+}
