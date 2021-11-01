7c292909d924d9a1a85f57b436d6f7f73b4bc6e2
theketan2 <ketantoo@gmail.com>
2021-11-01 13:10:42 +0530

    numbersToRanges

--- edabitJavascript/numbersToRanges.js
+++ edabitJavascript/numbersToRanges.js
@@ -1,26 +1,26 @@
 function numbersToRanges(arr) {
-    let sol = [arr[0]];
-    for (let i = 1; i < arr.length; i++) {
-        let last = sol.pop();
-        sol.push(last);
-        if (arr[i] - last !== 1) {
-            sol.push("#");
-            sol.push(arr[i]);
-        } else {
-            sol.push(arr[i]);
-        }
+  let sol = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    let last = sol.pop();
+    sol.push(last);
+    if (arr[i] - last !== 1) {
+      sol.push("#");
+      sol.push(arr[i]);
+    } else {
+      sol.push(arr[i]);
     }
-    sol = sol.join(",").split(",#,").map(item => item.split(",")).map(item => {
-        if (item.length === 1) {
-            return item[0];
-        } else {
-
-
-
-
-            return item[0] + "-" + item.pop();
-        }
-    })
-    console.log(sol)
-    return sol.filter(item => item !== "");
+  }
+  sol = sol
+    .join(",")
+    .split(",#,")
+    .map((item) => item.split(","))
+    .map((item) => {
+      if (item.length === 1) {
+        return item[0];
+      } else {
+        return item[0] + "-" + item.pop();
+      }
+    });
+  console.log(sol);
+  return sol.filter((item) => item !== "");
 }
