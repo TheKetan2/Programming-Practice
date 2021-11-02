9d304b2e29c3623178ae5f0905826283ab411f90
theketan2 <ketantoo@gmail.com>
2021-11-02 14:07:44 +0530

    leader

--- edabitJavascript/leader.js
+++ edabitJavascript/leader.js
@@ -1,17 +1,29 @@
 function leader(arr) {
-	let max = Math.max(...arr);
-	let sol = [];
-	let last= arr.pop();
-	while(last !== max){
-		sol.unshift(last);
-		last = arr.pop();
-	}
-	sol.unshift(last);
-	for(let i = 1; i<sol.length; i++){
-		if(sol[i-1] < sol[i]){
-			sol.splice(i-1, 1);
-		}
-	}
-	//console.log(sol)
-	return sol;
+  let max = Math.max(...arr);
+  let sol = [];
+  let last = arr.pop();
+  while (last !== max) {
+    sol.unshift(last);
+    last = arr.pop();
+  }
+  sol.unshift(last);
+  for (let i = 1; i < sol.length; i++) {
+    if (sol[i - 1] < sol[i]) {
+      sol.splice(i - 1, 1);
+    }
+  }
+  //console.log(sol)
+  return sol;
+}
+
+function leader(arr) {
+  let max = Math.max(...arr);
+  let sol = arr.slice(arr.lastIndexOf(max));
+  for (let i = 1; i < sol.length; i++) {
+    if (sol[i - 1] < sol[i]) {
+      sol.splice(i - 1, 1);
+    }
+  }
+  //console.log(sol)
+  return sol;
 }
