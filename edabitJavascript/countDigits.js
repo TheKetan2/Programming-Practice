0667bc68a532942ced81b94adeec00a7233be3ac
Ketan Ramteke <Ketantoo@gmail.com>
2022-01-19 23:08:50 +0530

    pairs

--- edabitJavascript/countDigits.js
+++ edabitJavascript/countDigits.js
@@ -1,16 +1,16 @@
 function countDigits(n, d) {
-	let sol = 0;
-	for(let i = 0; i<=n; i++){
-		let temp = (i*i);
-		if(temp==0 && d==0){
-			sol++;
-		}
-		while(temp){
-			if(d==temp%10){
-				sol++;
-			}
-			temp = Math.floor(temp/10);
-		}
-	}
-	return sol;
+  let sol = 0;
+  for (let i = 0; i <= n; i++) {
+    let temp = i * i;
+    if (temp == 0 && d == 0) {
+      sol++;
+    }
+    while (temp) {
+      if (d == temp % 10) {
+        sol++;
+      }
+      temp = Math.floor(temp / 10);
+    }
+  }
+  return sol;
 }
