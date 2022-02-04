5590b7926f11997b3261cbf473205dd127de94aa
Ketan Ramteke <Ketantoo@gmail.com>
2022-02-04 21:30:02 +0530

    getStudentsWithAndTopNotes

--- edabitJavascript/getStudentsWithNamesAndTopNotes.js
+++ edabitJavascript/getStudentsWithNamesAndTopNotes.js
@@ -1,7 +1,5 @@
-function getStudentsWithNamesAndTopNotes(students) {
-  let sol = students.map((obj) => {
+const getStudentsWithNamesAndTopNotes = (students) =>
+  students.map((obj) => {
     let { name, notes } = obj;
     return { name: name, topNote: Math.max(...notes, 0) };
   });
-  return sol;
-}
