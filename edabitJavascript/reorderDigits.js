ee9a7d1ac9e04c27b6af3f43c57eb5d437d74c3f
Ketan Ramteke <Ketantoo@gmail.com>
2022-02-12 21:43:22 +0530

    reorderDigits

--- edabitJavascript/reorderDigits.js
+++ edabitJavascript/reorderDigits.js
@@ -1,7 +1,12 @@
 function reorderDigits(arr, direction) {
-    let sol = [];
-    for (let num of arr) {
-        sol.push(Number(num.toString().split("").map(n => Number(n)).sort((a, b) => direction == "asc" ? a - b : b - a).join("")))
-    }
-    return sol;
+  return arr.map((num) =>
+    Number(
+      num
+        .toString()
+        .split("")
+        .map((n) => Number(n))
+        .sort((a, b) => (direction == "asc" ? a - b : b - a))
+        .join("")
+    )
+  );
 }
