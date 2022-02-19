db96de5ebbca38d0ee8948635f7f2f830b3b6987
Ketan Ramteke <Ketantoo@gmail.com>
2022-02-19 19:15:36 +0530

    notGOodCode

---
+++ edabitJavascript/preventChanges.js
@@ -0,0 +1,9 @@
+function preventChanges(obj) {
+  // write your code here
+	// don't use a return statement
+	
+	
+  // DON'T CHANGE OR REMOVE THE LINES BELOW
+  //obj.signature = "whatever";
+  return obj;
+}
