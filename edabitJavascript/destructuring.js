4d78ea20a3af7f8372b47d8effc3c89032c3ae99
Ketan Ramteke <Ketantoo@gmail.com>
2022-03-10 22:26:16 +0530

    destructuring

---
+++ edabitJavascript/destructuring.js
@@ -0,0 +1,10 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+first = arr[0]
+second = arr[1]
+third = arr[2]
+other = arr.slice(3)
+
+console.log(first)
+console.log(second) 
+console.log(third)
+console.log(other) 
