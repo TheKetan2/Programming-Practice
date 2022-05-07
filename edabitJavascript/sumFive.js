12592ac6ca2e0d69b15a4b1f6aad1b4773aec766
Ketan Ramteke <Ketantoo@gmail.com>
2022-05-07 21:08:00 +0530

    sumFive

--- edabitJavascript/sumFive.js
+++ edabitJavascript/sumFive.js
@@ -1,3 +1,5 @@
 function sumFive(arr) {
   return arr.filter((n) => n > 5).reduce((a, c) => a + c, 0);
 }
+const user = { name: "John", email: "john@example.com",city: "Phoenix", state: "AZ", country: "USA"}
+const str = `({ name, email,...rest} = user ).toString()`
