262c4126128413a9d9d865eda2fe29d6b43621ef
Ketan Ramteke <Ketantoo@gmail.com>
2022-08-08 22:34:35 +0530

    gradePercentage

---
+++ edabitJavascript/gradePercentage.js
@@ -0,0 +1,6 @@
+function gradePercentage(userScore, passScore) {
+  userScore = parseInt(userScore.substring(0, userScore.length - 1));
+  passScore = parseInt(passScore.substring(0, passScore.length - 1));
+  let result = userScore < passScore ? "FAILED" : "PASSED";
+  return "You " + result + " the Exam";
+}
