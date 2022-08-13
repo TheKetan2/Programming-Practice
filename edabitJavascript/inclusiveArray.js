2fbabce8b5933abdcf7def43954df7bfbe5ba3df
Ketan Ramteke <Ketantoo@gmail.com>
2022-08-13 12:11:47 +0530

    inclusiveArray

---
+++ edabitJavascript/inclusiveArray.js
@@ -0,0 +1,10 @@
+function inclusiveArray(startNum, endNum) {
+	let sol = [];
+	if(startNum>=endNum){
+		return [startNum];
+	}
+	for(let i = startNum; i<=endNum;i++){
+		sol.push(i);
+	}
+	return sol;
+}
