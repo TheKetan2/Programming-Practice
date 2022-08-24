2e3b81154f068916596a83612f24c95c889a15d6
Ketan Ramteke <Ketantoo@gmail.com>
2022-08-24 16:54:07 +0530

    dartScoring

--- edabitJavascript/dartsScoring.js
+++ edabitJavascript/dartsScoring.js
@@ -1,12 +1,4 @@
 function dartsScoring(x, y) {
-	let dist = Math.pow((x*x + y*y),0.5);
-	if(dist <=1){
-		return 10;
-	}else if(dist <=5){
-		return 5;
-	}else if(dist <=10){
-		return 1;
-	}else{
-		return 0;
-	}
+  let dist = Math.pow(x * x + y * y, 0.5);
+  return dist <= 1 ? 10 : dist <= 5 ? 5 : dist <= 10 ? 1 : 0;
 }
