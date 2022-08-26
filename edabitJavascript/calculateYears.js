3271f461d5d62b231cad1b16518d16c81641efd9
Ketan Ramteke <Ketantoo@gmail.com>
2022-08-26 20:23:23 +0530

    humanYears

--- edabitJavascript/calculateYears.js
+++ edabitJavascript/calculateYears.js
@@ -1,17 +1,10 @@
 function calculateYears(humanYears) {
-	let [h,c,d] =[humanYears, 0, 0];
-	for(let i = 1; i<=humanYears;i++ ){
-		if(i==1){
-			c+=15;
-			d+=15;
-		}else if(i==2){
-			c+=9;
-			d+=9;
-		}else{
-			c+=((humanYears-2)*4);
-			d+=((humanYears-2)*5);
-			return [h,c,d];
-		}
-	}
-	return [h,c,d];
+  let [h, c, d] = [humanYears, 0, 0];
+  if (h > 2) {
+    return [h, 24 + (h - 2) * 4, 24 + (h - 2) * 5];
+  } else if (h == 2) {
+    return [h, 24, 24];
+  } else {
+    return [h, 15, 15];
+  }
 }
